42b346c3aa2bfc0f380d321d6c886d7ba1df88c9
Maddy <madhurima.dey@leapdev.io>
2018-11-13 17:02:26 +1100

    adding logic to download content of current page into mhtml

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {} from "chrome";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  text: string = "Initial";
+
+
+  onSave() {
+    this.text = 'saved'
+    var that = this;
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+      console.log('inside');
+      that.text = 'inside';
+      that.saveCurrentPage(tabs[0].id);
+    });
+  }
+
+saveCurrentPage(tabId) {
+    chrome.pageCapture.saveAsMHTML({
+      tabId: tabId
+    }, function (blob) {
+      var url = URL.createObjectURL(blob);
+      // Optional: chrome.tabs.remove(tabId); // to close the tab
+      chrome.downloads.download({
+        url: url,
+        filename: 'whatever.mhtml'
+      });
+    });
+  }
+
 }
